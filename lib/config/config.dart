553d881af3aa91efedbf6013453e3209295aac8f
Faysal Neowaz <faysalneowaz@.com>
2021-08-25 15:28:43 +0600

    first commit

---
+++ lib/config/config.dart
@@ -0,0 +1,13 @@
+class Config {
+  final String appName = 'New Prof'; //app name
+  final String testerPassword =
+      'F@ysal63365'; //testing password - don't use this password in the database (you can change this testing password too)
+
+  //firebase server token for push notication
+  final String serverToken =
+      'AAAAKc5b2us:APA91bGh0Gp8VVwKt2y4bZEIsjvrEMHxV6PDRcw8cWYyNttdm2XBvS9CkyfF1D1w66nWo-ui9fZclREM0K0NvAQ43YIbG2P3aTy_c1aGRyiKdEMk7OVi6ajcmcTk9aOZGALCIv0b9OTq';
+  final String icon = 'assets/images/icon.png'; // app icon
+
+  //don't edit or remove this
+  final List contentTypes = ['image', 'video'];
+}
